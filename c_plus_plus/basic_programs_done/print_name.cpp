1412ae037ed92b72bb8c5683baa788096d234599
Dwight Mathews <dwightmathews30@gmail.com>
2025-03-31 15:50:05 -0400

    second

--- c_plus_plus/basic_programs_done/print_name.cpp
+++ c_plus_plus/basic_programs_done/print_name.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-int main() {
-    cout << "Hello World";
+
+int main(){
+    cout << "Dwight Mathews" << endl;
     return 0;
 }
---
+++ c_plus_plus/basic_programs_done/user_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int user_input;
+
+    cin >> user_input;
+
+    cout << user_input;
+
+    return 0;
+}
