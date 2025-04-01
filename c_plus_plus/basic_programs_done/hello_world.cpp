632bb775120de07ac0f8b2cfcc709a67cb10556a
Dwight Mathews <dwightmathews30@gmail.com>
2025-03-31 21:39:17 -0400

    john jones

--- c_plus_plus/basic_programs_done/hello_world.cpp
+++ c_plus_plus/basic_programs_done/hello_world.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main() {
     cout << "Hello World" << endl;
-    cout.flush();
+    cin.get();
     return 0;
 }
--- c_plus_plus/basic_programs_done/print_name.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "Dwight Mathews" << endl;
-    return 0;
-}
